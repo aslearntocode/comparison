34303686131fd6634d9382239ab065af127770f2
aslearntocode <aslearntocode@gmail.com>
2025-05-24 16:13:27 +0530

    Edited Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -664,7 +664,7 @@ export default function Home() {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
               </svg>
               <span className="text-base md:text-lg font-medium">
-                Explore freely, there is no hidden subscription fee, walk away if you find one. &nbsp;&nbsp;&nbsp;
+                Explore freely! There is no hidden subscription fee, walk away if you find one. &nbsp;&nbsp;&nbsp;
               </span>
             </span>
             {/* Second set */}
@@ -684,6 +684,15 @@ export default function Home() {
                 Apply through us and get INR 100-1000 reward on successful application! &nbsp;&nbsp;&nbsp;
               </span>
             </span>
+            {/* Third message - new */}
+            <span className="mx-4 flex items-center">
+              <svg className="w-6 h-6 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
+              </svg>
+              <span className="text-base md:text-lg font-medium">
+                Explore freely! There is no hidden subscription fee, walk away if you find one. &nbsp;&nbsp;&nbsp;
+              </span>
+            </span>
             {/* Third set */}
             <span className="mx-4 flex items-center">
               <svg className="w-6 h-6 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -699,6 +708,15 @@ export default function Home() {
               </svg>
               <span className="text-base md:text-lg font-medium">
                 Apply through us and get INR 100-1000 reward on successful application! &nbsp;&nbsp;&nbsp;
+              </span>
+            </span>
+            {/* Third message - new */}
+            <span className="mx-4 flex items-center">
+              <svg className="w-6 h-6 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
+              </svg>
+              <span className="text-base md:text-lg font-medium">
+                Explore freely! There is no hidden subscription fee, walk away if you find one. &nbsp;&nbsp;&nbsp;
               </span>
             </span>
           </div>
